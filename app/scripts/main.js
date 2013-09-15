260c7119a2b20e5399d86e83bfe739c692ddfd38
alisonelizabeth <alisonmllr20@gmail.com>
2013-09-15 16:33:56 -0400

    Working on edit function, added modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,30 @@ var tasks = new TaskClassCollection();
 $('document').ready(function() {
 	$('.complete h1').hide();
 	fetchTaskCollection(tasks);
+	addTask();	
+	completeTask();
+	deleteTask();	
+}); 
+// end of document ready 
+
+// functions begin 
+// fetches TaskCollection 
+function fetchTaskCollection(tasks) {
+	tasks.fetch({
+		success: function(collection) {
+			collection.each(function(task){
+				addToTaskList(task);
+				complete(task);
+		});
+	},
+		error: function(collection, error) {
+			console.log(error.description);
+	}
+});
+};
+
+// saves task  
+function addTask () {
 	$('.add').click(function(event){
 	event.preventDefault();
 	var task = new TaskClass();
@@ -30,7 +54,11 @@ $('document').ready(function() {
 	});
 });
 
-	$('.completed').click(function(){
+}
+
+// complete task and saves sets property isComplete to true  
+function completeTask() {
+$('.completed').click(function(){
 		var id = $('input:checked').val();
 		var query = new Parse.Query(TaskClass);
 		query.equalTo('objectId', id);
@@ -45,7 +73,10 @@ $('document').ready(function() {
 			}
 		});
 	});
+}
 
+// deletes task from Parse 
+function deleteTask () {
 	$('.delete').click(function() {
 		var id = $('input:checked').val();
 		var query = new Parse.Query(TaskClass);
@@ -60,26 +91,8 @@ $('document').ready(function() {
 			}
 		});
 	});
-}); 
-// end of document ready 
+}
 
-
-// functions begin 
-
-// fetches TaskCollection 
-function fetchTaskCollection(tasks) {
-	tasks.fetch({
-		success: function(collection) {
-			collection.each(function(task){
-				addToTaskList(task);
-				complete(task);
-		});
-	},
-		error: function(collection, error) {
-			console.log(error.description);
-	}
-});
-};
 
 // adds incompleted tasks to task list 
 function addToTaskList(task) {
@@ -90,7 +103,7 @@ function addToTaskList(task) {
 		}
 	}
 	
-// adds completed tasks to 'done that' section
+// adds completed tasks to #completed-list div 
 function complete (task) {
   	var li = $('<li>'+ '<span class="glyphicon glyphicon-thumbs-up">' + '</span>' + task.get('task') + '</li>');
   		console.log(task);
@@ -103,19 +116,50 @@ function complete (task) {
 }
 
 
-// $('.edit').click(function(){
-// 	var id = $('input:checked').val()
-// 	var query = new Parse.Query(TaskClass);
-// 	var inputVal = $('#form-input').val();
-// 	query.equalTo('objectId', id);
-// 	query.find({
-// 		success: function(result) {
+$('.save-edit').click(function(){	
+	var id = $('input:checked').val()
+	var query = new Parse.Query(TaskClass);
+	var inputVal = $('#edit-text').val();
+	query.equalTo('objectId', id);
+	query.find({
+		success: function(result) {
+			result[0].set('task',inputVal); 
+			result[0].save();
+		},
+		error: function(result, error) {
+			console.log(error.description)
+		}
+	})	
+})
 
-// 			result[0].set('task',inputVal); 
-// 			result[0].save();
+
+
+
+// function addTask () {
+// 	$('.add').click(function(event){
+// 	event.preventDefault();
+// 	var task = new TaskClass();
+// 	var inputVal = $('#form-input').val();
+// 	task.set('task', inputVal);
+// 	task.set('isComplete', false);
+
+// 	task.save(null, {
+// 		success: function(result){
+// 			console.log('it worked');
+// 			addToTaskList(result);
+// 			$('#form-input').val('');
 // 		},
-// 		error: function(result, error) {
-// 			console.log(error.description)
+// 		error: function(result, error){
+// 			console.log(error.description);
 // 		}
-// 	})	
-// })
+// 	});
+// });
+
+// }
+
+
+
+
+
+
+
